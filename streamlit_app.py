ae8a35e7c0613fbf7b880d29ae2884ff1a1646eb
toto913 <163992506+toto913@users.noreply.github.com>
2024-03-19 16:11:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-st.write(my_dataframe.show())
+st.write(my_dataframe['FRUIT_NAME'])
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
